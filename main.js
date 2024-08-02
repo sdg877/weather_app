3bd5dec06efa32ed258bdf2bbfaf68359adf3c2e
Sdg877 <sdrakegill@gmail.com>
2024-08-02 13:00:15 +0100

    api added and functions to dislay daily data

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-import './style.css'
+import "./style.css"
+import { getWeather } from "./weather"
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+.then(renderWeather)
+.catch(e => {
+    console.error(e)
+    alert("Error getting weather")
+})
+
+function renderWeather({ current, daily, hourly }) {
+    renderCurrentWeather(current)
+    // renderDailyWeather(daily)
+    // renderHourlyWeather(hourly)
+    document.body.classList.remove("blurred")
+}
+
+function renderCurrentWeather(current) {
+    document.querySelector("[data-current-temp").textContent = current.currentTemp
+}
---
+++ weather.js
@@ -0,0 +1,93 @@
+
+import axios from "axios";
+
+export function getWeather(lat, lon, timezone) {
+    return axios.get(
+        "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime", 
+        {
+            params: {
+                latitude: lat,
+                longitude: lon,
+                timezone,
+            },
+        }
+    ).then(({ data }) => {
+        return {
+            current: parseCurrentWeather(data),
+            daily: parseDailyWeather(data),
+            hourly: parseHourlyWeather(data)
+        };
+    });
+}
+
+function parseCurrentWeather({ current_weather, daily }) {
+    if (!current_weather || !daily) {
+        return {
+            currentTemp: null,
+            highTemp: null,
+            lowTemp: null,
+            highFeelsLike: null,
+            lowFeelsLike: null,
+            windSpeed: null,
+            precip: null,
+            iconCode: null,
+        };
+    }
+
+    const {
+        temperature: currentTemp = null,
+        windspeed: windSpeed = null,
+        weathercode: iconCode = null,
+    } = current_weather;
+
+    const {
+        temperature_2m_max: [maxTemp] = [null],
+        temperature_2m_min: [minTemp] = [null],
+        apparent_temperature_max: [maxFeelsLike] = [null],
+        apparent_temperature_min: [minFeelsLike] = [null],
+        precipitation_sum: [precip] = [null],
+    } = daily;
+
+    return {
+        currentTemp: currentTemp !== null ? Math.round(currentTemp) : null,
+        highTemp: maxTemp !== null ? Math.round(maxTemp) : null,
+        lowTemp: minTemp !== null ? Math.round(minTemp) : null,
+        highFeelsLike: maxFeelsLike !== null ? Math.round(maxFeelsLike) : null,
+        lowFeelsLike: minFeelsLike !== null ? Math.round(minFeelsLike) : null,
+        windSpeed: windSpeed !== null ? Math.round(windSpeed) : null,
+        precip: precip !== null ? Math.round(precip * 100) / 100 : null,
+        iconCode,
+    };
+}
+
+function parseDailyWeather({ daily }) {
+    if (!daily || !daily.time) {
+        return [];
+    }
+    return daily.time.map((time, index) => {
+        return {
+            timestamp: time * 1000,
+            iconCode: daily.weathercode[index],
+            maxTemp: Math.round(daily.temperature_2m_max[index]),
+            minTemp: Math.round(daily.temperature_2m_min[index]),
+            maxFeelsLike: Math.round(daily.apparent_temperature_max[index]),
+            minFeelsLike: Math.round(daily.apparent_temperature_min[index]),
+        };
+    });
+}
+
+function parseHourlyWeather({ hourly, current_weather }) {
+    if (!hourly || !hourly.time) {
+        return [];
+    }
+    return hourly.time.map((time, index) => {
+        return {
+            timestamp: time * 1000,
+            iconCode: hourly.weathercode[index],
+            temp: Math.round(hourly.temperature_2m[index]),
+            feelsLike: Math.round(hourly.apparent_temperature[index]),
+            windSpeed: Math.round(hourly.windspeed_10m[index]),
+            precip: Math.round(hourly.precipitation[index] * 100) / 100,
+        };
+    }).filter(({ timestamp }) => current_weather && timestamp >= current_weather.time * 1000);
+}
