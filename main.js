0e97031f7973e352bf4dd24726ee0aeac8a33cff
Sdg877 <sdrakegill@gmail.com>
2024-10-29 13:32:29 +0000

    Update to include postcode search, UV index and sunrise and sunset times

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import "./style.css"
-import { getWeather } from "./weather"
-import { ICON_MAP } from "./iconMap"
+import "./style.css";
+import { getWeather } from "./weather";
+import { ICON_MAP } from "./iconMap";
 
-navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
+navigator.geolocation.getCurrentPosition(positionSuccess, positionError);
 
 function positionSuccess({ coords }) {
   getWeather(
@@ -11,73 +11,143 @@ function positionSuccess({ coords }) {
     Intl.DateTimeFormat().resolvedOptions().timeZone
   )
     .then(renderWeather)
-    .catch(e => {
-      console.error(e)
-      alert("Error getting weather.")
-    })
+    .catch((e) => {
+      console.error(e);
+      alert("Error getting weather.");
+    });
 }
 
 function positionError() {
   alert(
     "There was an error getting your location. Please allow us to use your location and refresh the page."
-  )
+  );
 }
 
 function renderWeather({ current, daily, hourly }) {
-  renderCurrentWeather(current)
-  renderDailyWeather(daily)
-  renderHourlyWeather(hourly)
-  document.body.classList.remove("blurred")
+  renderCurrentWeather(current);
+  renderDailyWeather(daily);
+  renderHourlyWeather(hourly);
+  document.body.classList.remove("blurred");
 }
 
 function setValue(selector, value, { parent = document } = {}) {
-  parent.querySelector(`[data-${selector}]`).textContent = value
+  parent.querySelector(`[data-${selector}]`).textContent = value;
 }
 
 function getIconUrl(iconCode) {
-  return `icons/${ICON_MAP.get(iconCode)}.svg`
+  return `icons/${ICON_MAP.get(iconCode)}.svg`;
 }
 
-const currentIcon = document.querySelector("[data-current-icon]")
+const currentIcon = document.querySelector("[data-current-icon]");
 function renderCurrentWeather(current) {
-  currentIcon.src = getIconUrl(current.iconCode)
-  setValue("current-temp", current.currentTemp)
-  setValue("current-high", current.highTemp)
-  setValue("current-low", current.lowTemp)
-  setValue("current-fl-high", current.highFeelsLike)
-  setValue("current-fl-low", current.lowFeelsLike)
-  setValue("current-wind", current.windSpeed)
-  setValue("current-precip", current.precip)
+  currentIcon.src = getIconUrl(current.iconCode);
+  setValue("current-temp", current.currentTemp);
+  setValue("current-high", current.highTemp);
+  setValue("current-low", current.lowTemp);
+  setValue("current-fl-high", current.highFeelsLike);
+  setValue("current-fl-low", current.lowFeelsLike);
+  setValue("current-wind", current.windSpeed);
+  setValue("current-precip", current.precip);
+  setValue("current-uv-index", current.uvIndex);
+  setValue("current-sunrise", formatTime(current.sunrise));
+  setValue("current-sunset", formatTime(current.sunset));
 }
 
-const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" })
-const dailySection = document.querySelector("[data-day-section]")
-const dayCardTemplate = document.getElementById("day-card-template")
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" });
+const dailySection = document.querySelector("[data-day-section]");
+const dayCardTemplate = document.getElementById("day-card-template");
 function renderDailyWeather(daily) {
-  dailySection.innerHTML = ""
-  daily.forEach(day => {
-    const element = dayCardTemplate.content.cloneNode(true)
-    setValue("temp", day.maxTemp, { parent: element })
-    setValue("date", DAY_FORMATTER.format(day.timestamp), { parent: element })
-    element.querySelector("[data-icon]").src = getIconUrl(day.iconCode)
-    dailySection.append(element)
-  })
+  dailySection.innerHTML = "";
+  daily.forEach((day) => {
+    const element = dayCardTemplate.content.cloneNode(true);
+    setValue("temp", day.maxTemp, { parent: element });
+    setValue("date", DAY_FORMATTER.format(day.timestamp), { parent: element });
+    element.querySelector("[data-icon]").src = getIconUrl(day.iconCode);
+    dailySection.append(element);
+  });
 }
 
-const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, { hour: "numeric" })
-const hourlySection = document.querySelector("[data-hour-section]")
-const hourRowTemplate = document.getElementById("hour-row-template")
+const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, { hour: "numeric" });
+const hourlySection = document.querySelector("[data-hour-section]");
+const hourRowTemplate = document.getElementById("hour-row-template");
 function renderHourlyWeather(hourly) {
-  hourlySection.innerHTML = ""
-  hourly.forEach(hour => {
-    const element = hourRowTemplate.content.cloneNode(true)
-    setValue("temp", hour.temp, { parent: element })
-    setValue("fl-temp", hour.feelsLike, { parent: element })
-    setValue("wind", hour.windSpeed, { parent: element })
-    setValue("precip", hour.precip, { parent: element })
-    setValue("day", DAY_FORMATTER.format(hour.timestamp), { parent: element })
-    setValue("time", HOUR_FORMATTER.format(hour.timestamp), { parent: element })
-    element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode)
-    hourlySection.append(element)
-  })
+  hourlySection.innerHTML = "";
+  hourly.forEach((hour) => {
+    const element = hourRowTemplate.content.cloneNode(true);
+    setValue("temp", hour.temp, { parent: element });
+    setValue("fl-temp", hour.feelsLike, { parent: element });
+    setValue("wind", hour.windSpeed, { parent: element });
+    setValue("precip", hour.precip, { parent: element });
+    setValue("day", DAY_FORMATTER.format(hour.timestamp), { parent: element });
+    setValue("time", HOUR_FORMATTER.format(hour.timestamp), {
+      parent: element,
+    });
+    element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode);
+    hourlySection.append(element);
+  });
+}
+
+const postcodeInput = document.getElementById("postcode-input");
+const searchButton = document.getElementById("search-button");
+
+async function getCoordinatesFromPostcode(postcode) {
+  const postcodeRegex = /^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$/i;
+  if (!postcodeRegex.test(postcode)) {
+    throw new Error("Invalid postcode format");
+  }
+
+  try {
+    const response = await fetch(
+      `https://api.opencagedata.com/geocode/v1/json?q=${postcode}&key=0170d0b1733440318d03be612c8df659`
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Geocoding API request failed with status ${response.status}`
+      );
+    }
+
+    const data = await response.json();
+
+    if (data.results.length > 0) {
+      const { lat, lng } = data.results[0].geometry;
+      return { latitude: lat, longitude: lng };
+    } else {
+      throw new Error("Postcode not found. Please check your input.");
+    }
+  } catch (error) {
+    console.error("Error geocoding postcode:", error);
+    throw new Error("Error geocoding postcode");
+  }
+}
+
+searchButton.addEventListener("click", () => {
+  const postcode = postcodeInput.value.trim();
+
+  getCoordinatesFromPostcode(postcode)
+    .then(({ latitude, longitude }) => {
+      getWeather(
+        latitude,
+        longitude,
+        Intl.DateTimeFormat().resolvedOptions().timeZone
+      )
+        .then(renderWeather)
+        .catch((e) => {
+          console.error(e);
+          alert("Error getting weather.");
+        });
+    })
+    .catch((e) => {
+      console.error(e);
+      alert(e.message);
+    });
+});
+
+
+function formatTime(timestamp) {
+  const date = new Date(timestamp);
+  const hours = date.getHours() % 12 || 12;
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const ampm = date.getHours() < 12 ? "AM" : "PM";
+  return `${hours}:${minutes} ${ampm}`;
 }
--- weather.js
+++ weather.js
@@ -1,93 +1,117 @@
 import axios from "axios";
 
 export function getWeather(lat, lon, timezone) {
-    return axios.get(
-        "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime", 
-        {
-            params: {
-                latitude: lat,
-                longitude: lon,
-                timezone,
-            },
-        }
-    ).then(({ data }) => {
-        const weatherData = Array.isArray(data) ? data[0] : data; 
-        return {
-            current: parseCurrentWeather(weatherData),
-            daily: parseDailyWeather(weatherData),
-            hourly: parseHourlyWeather(weatherData)
-        };
+  return axios
+    .get("https://api.open-meteo.com/v1/forecast", {
+      params: {
+        latitude: lat,
+        longitude: lon,
+        timezone,
+        current_weather: true,
+        hourly:
+          "temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m",
+        daily:
+          "weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum,uv_index_max,sunrise,sunset",
+        wind_speed_unit: "mph",
+        precipitation_unit: "inch",
+        timeformat: "unixtime",
+      },
+    })
+    .then(({ data }) => {
+      const weatherData = Array.isArray(data) ? data[0] : data;
+      return {
+        current: parseCurrentWeather(weatherData),
+        daily: parseDailyWeather(weatherData),
+        hourly: parseHourlyWeather(weatherData),
+      };
     });
 }
 
 function parseCurrentWeather({ current_weather, daily }) {
-    if (!current_weather || !daily) {
-        return {
-            currentTemp: null,
-            highTemp: null,
-            lowTemp: null,
-            highFeelsLike: null,
-            lowFeelsLike: null,
-            windSpeed: null,
-            precip: null,
-            iconCode: null,
-        };
-    }
-
-    const {
-        temperature: currentTemp = null,
-        windspeed: windSpeed = null,
-        weathercode: iconCode = null,
-    } = current_weather;
-
-    const {
-        temperature_2m_max: [maxTemp] = [null],
-        temperature_2m_min: [minTemp] = [null],
-        apparent_temperature_max: [maxFeelsLike] = [null],
-        apparent_temperature_min: [minFeelsLike] = [null],
-        precipitation_sum: [precip] = [null],
-    } = daily;
-
+  if (!current_weather || !daily) {
     return {
-        currentTemp: currentTemp !== null ? Math.round(currentTemp) : null,
-        highTemp: maxTemp !== null ? Math.round(maxTemp) : null,
-        lowTemp: minTemp !== null ? Math.round(minTemp) : null,
-        highFeelsLike: maxFeelsLike !== null ? Math.round(maxFeelsLike) : null,
-        lowFeelsLike: minFeelsLike !== null ? Math.round(minFeelsLike) : null,
-        windSpeed: windSpeed !== null ? Math.round(windSpeed) : null,
-        precip: precip !== null ? Math.round(precip * 100) / 100 : null,
-        iconCode,
+      currentTemp: null,
+      highTemp: null,
+      lowTemp: null,
+      highFeelsLike: null,
+      lowFeelsLike: null,
+      windSpeed: null,
+      precip: null,
+      iconCode: null,
+      uvIndex: null,
+      sunrise: null,
+      sunset: null,
     };
+  }
+
+  const {
+    temperature: currentTemp = null,
+    windspeed: windSpeed = null,
+    weathercode: iconCode = null,
+  } = current_weather;
+
+  const {
+    temperature_2m_max: [maxTemp] = [null],
+    temperature_2m_min: [minTemp] = [null],
+    apparent_temperature_max: [maxFeelsLike] = [null],
+    apparent_temperature_min: [minFeelsLike] = [null],
+    precipitation_sum: [precip] = [null],
+    uv_index_max: [uvIndex] = [null],
+    sunrise: [sunrise] = [null],
+    sunset: [sunset] = [null],
+  } = daily;
+
+  return {
+    currentTemp: currentTemp !== null ? Math.round(currentTemp) : null,
+    highTemp: maxTemp !== null ? Math.round(maxTemp) : null,
+    lowTemp: minTemp !== null ? Math.round(minTemp) : null,
+    highFeelsLike: maxFeelsLike !== null ? Math.round(maxFeelsLike) : null,
+    lowFeelsLike: minFeelsLike !== null ? Math.round(minFeelsLike) : null,
+    windSpeed: windSpeed !== null ? Math.round(windSpeed) : null,
+    precip: precip !== null ? Math.round(precip * 100) / 100 : null,
+    iconCode,
+    uvIndex,
+    sunrise: sunrise * 1000,
+    sunset: sunset * 1000,
+  };
 }
 
 function parseDailyWeather({ daily }) {
-    if (!daily || !daily.time) {
-        return [];
-    }
-    return daily.time.map((time, index) => {
-        return {
-            timestamp: time * 1000,
-            iconCode: daily.weathercode[index],
-            maxTemp: Math.round(daily.temperature_2m_max[index]),
-            minTemp: Math.round(daily.temperature_2m_min[index]),
-            maxFeelsLike: Math.round(daily.apparent_temperature_max[index]),
-            minFeelsLike: Math.round(daily.apparent_temperature_min[index]),
-        };
-    });
+  if (!daily || !daily.time) {
+    return [];
+  }
+  return daily.time.map((time, index) => {
+    return {
+      timestamp: time * 1000,
+      iconCode: daily.weathercode[index],
+      maxTemp: Math.round(daily.temperature_2m_max[index]),
+      minTemp: Math.round(daily.temperature_2m_min[index]),
+      maxFeelsLike: Math.round(daily.apparent_temperature_max[index]),
+      minFeelsLike: Math.round(daily.apparent_temperature_min[index]),
+      uvIndex: daily.uv_index_max[index],
+      sunrise: daily.sunrise[index] * 1000,
+      sunset: daily.sunset[index] * 1000,
+    };
+  });
 }
 
 function parseHourlyWeather({ hourly, current_weather }) {
-    if (!hourly || !hourly.time) {
-        return [];
-    }
-    return hourly.time.map((time, index) => {
-        return {
-            timestamp: time * 1000,
-            iconCode: hourly.weathercode[index],
-            temp: Math.round(hourly.temperature_2m[index]),
-            feelsLike: Math.round(hourly.apparent_temperature[index]),
-            windSpeed: Math.round(hourly.windspeed_10m[index]),
-            precip: Math.round(hourly.precipitation[index] * 100) / 100,
-        };
-    }).filter(({ timestamp }) => current_weather && timestamp >= current_weather.time * 1000);
+  if (!hourly || !hourly.time) {
+    return [];
+  }
+  return hourly.time
+    .map((time, index) => {
+      return {
+        timestamp: time * 1000,
+        iconCode: hourly.weathercode[index],
+        temp: Math.round(hourly.temperature_2m[index]),
+        feelsLike: Math.round(hourly.apparent_temperature[index]),
+        windSpeed: Math.round(hourly.windspeed_10m[index]),
+        precip: Math.round(hourly.precipitation[index] * 100) / 100,
+      };
+    })
+    .filter(
+      ({ timestamp }) =>
+        current_weather && timestamp >= current_weather.time * 1000
+    );
 }
