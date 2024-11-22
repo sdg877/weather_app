4907450494e9f99c1bbef30caab11a1a288fbff5
Sdg877 <sdrakegill@gmail.com>
2024-11-22 06:50:46 +0000

    Updating image paths

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function setValue(selector, value, { parent = document } = {}) {
 }
 
 function getIconUrl(iconCode) {
-  return `icons/${ICON_MAP.get(iconCode)}.svg`;
+  return `/public/icons/${ICON_MAP.get(iconCode)}.svg`;
 }
 
 const currentIcon = document.querySelector("[data-current-icon]");
